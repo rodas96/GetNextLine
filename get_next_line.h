6fecb91b25a1534d0e91d0912b1164c1ca48c323
rodas96 <rodrigomiguelmorais@gmail.com>
2022-11-19 22:01:44 +0000

    Started

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmorais <rmorais@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/18 23:31:29 by rmorais           #+#    #+#             */
+/*   Updated: 2022/11/19 21:58:51 by rmorais          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE 1
+#define BUFFFER_SIZE 1
+#endif
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
